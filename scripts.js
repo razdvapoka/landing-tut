e3fdb729171c63fd2ea0117b3f90fb758ffed90d
razdvapoka <razdvapoka@gmail.com>
2019-11-27 21:16:29 +0300

    Add generative menu

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 console.log("hey!");
+
+/*
+for(let i = 0; i < 10000; i++) {
+  menuItems.push({ id: i });
+}
+*/
+
+var menuItems = [
+  {
+    id: "section-1"
+  },
+  {
+    id: "section-2"
+  },
+  {
+    id: "section-3"
+  }
+];
+
+var menu = document.querySelector("nav > ul");
+console.log(menu);
+
+function createMenuItemElement(menuItem) {
+  var menuItemElement = document.createElement("li");
+  menuItemElement.innerText = menuItem.id;
+
+  var section = document.getElementById(menuItem.id);
+  menuItemElement.addEventListener('click', function () {
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  menu.appendChild(menuItemElement);
+}
+
+menuItems.forEach(createMenuItemElement);
