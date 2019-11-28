3f52e500880c3194ac43a8b14a167679eae51a2c
razdvapoka <razdvapoka@users.noreply.github.com>
2019-11-28 17:38:08 +0300

    Update scripts.js
    Add script comments

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ for(let i = 0; i < 10000; i++) {
 }
 */
 
+// объявляем массив пунктов меню
 var menuItems = [
   {
     id: "section-1"
@@ -18,19 +19,28 @@ var menuItems = [
   }
 ];
 
+// находим элемент меню
 var menu = document.querySelector("nav > ul");
 console.log(menu);
 
+// объявляем функцию createMenuItemElement
 function createMenuItemElement(menuItem) {
+  // создаём новый элемент <li>
   var menuItemElement = document.createElement("li");
+  // ставим ему внутрь текст из menuItem.id
   menuItemElement.innerText = menuItem.id;
 
+  // находим элемент по id
   var section = document.getElementById(menuItem.id);
+  // добавляем обработчик события клик созданному выше элементу <li>
   menuItemElement.addEventListener('click', function () {
+    // скроллим до элемента section
     section.scrollIntoView({ behavior: 'smooth' });
   });
 
+  // добавляем созданный выше элемент в меню
   menu.appendChild(menuItemElement);
 }
 
+// для каждого элемента массива menuItems вызываем функцию createMenuItemElement
 menuItems.forEach(createMenuItemElement);
